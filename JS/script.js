4965731fa489203298209d50d736b7a058d7b44c
llucixxl <138773138+llucixxl@users.noreply.github.com>
2024-09-06 17:15:53 +0300

    Add functions for each operation required (add, subtract, multiply, divide)

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,35 @@
 // Main algorithm code for the calculator
+
+// Functions for the add, subtract, multiply and divide operations
+
+// Add operation:
+
+function add(a, b) {
+  return a + b;
+}
+
+// Subtract operation:
+
+function subtract(a, b) {
+  return a - b;
+}
+
+// Multiply operation:
+
+function multiply(a, b) {
+  return a * b;
+}
+
+// Divide operation:
+
+function divide(a, b) {
+  return a / b;
+}
+
+// End of operation functions
+
+// Variable for each part of the operation: number(operand), operator, number(operand)
+
+let number1;
+let operator;
+let number2;
